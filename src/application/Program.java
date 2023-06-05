2e50f22c9b344ecfa8256827ae53592333a0d28a
Wilton Jr <wilton007@hotmail.com>
2023-06-05 09:46:03 -0300

    comçando a implementação: tabuleiro e peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import tabuleiroJogo.Posicao;
+import tabuleiroJogo.Tabuleiro;
 
 public class Program {
     public static void main(String[] args) {
-        Posicao posicao = new Posicao(3,5);
-        System.out.println(posicao);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
 }
