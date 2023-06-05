9697a82fb568919de3609341f23f4f20ed020e36
Wilton Jr <wilton007@hotmail.com>
2023-06-05 10:09:59 -0300

    Camada do xadrez e print do tabluleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import tabuleiroJogo.Posicao;
 import tabuleiroJogo.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Program {
     public static void main(String[] args) {
-        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+        PartidaXadrez partidaXadrez = new PartidaXadrez();
+        UI.printTabuleiro(partidaXadrez.getPecas());
     }
 }
---
+++ src/xadrez/PecaXadrez.java
@@ -0,0 +1,18 @@
+package xadrez;
+
+import tabuleiroJogo.Peca;
+import tabuleiroJogo.Tabuleiro;
+
+public class PecaXadrez extends Peca {
+
+    private Cor cor;
+
+    public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro);
+        this.cor = cor;
+    }
+
+    public Cor getCor() {
+        return cor;
+    }
+}
